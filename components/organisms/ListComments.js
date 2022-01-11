fc1170d8d98cd4c5b9d5ec93afed4d709a02096a
Kawai cheung <kawaicheungdev@gmail.com>
2022-01-11 16:18:10 +0100

    add key list

--- components/organisms/ListComments.js
+++ components/organisms/ListComments.js
@@ -4,8 +4,8 @@ import Comment from "../molecules/Comment";
 export default function ListComments({ data }) {
   return (
     <div>
-      {data.slice(0, 3).map((x) => (
-        <Comment comment={x.attributes}></Comment>
+      {data.slice(0, 3).map((x, index) => (
+        <Comment key={index} comment={x.attributes}></Comment>
       ))}
     </div>
   );
